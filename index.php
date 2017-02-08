39a1177c36f49e915cedb0561bf5f2d56b0deb7d
Nathan <nathan@clevid.com>
2017-02-08 16:33:21 -0600

    Added menu and some content

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <?php require 'php/hat.php'; ?>
 
-<div class="row">
+
 
 
 
 
     <div class="row">
-      <h2 class="stitle2">Services</h2>
-    </div>
+      <div class="col-md-12">
+        <h2 class="stitle2">Services</h2>
+      </div>
+
+
 
       <div class="col-md-4">
         <h3 class="ctitle">Web Design</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Let us bring your vision to life with our modern style and innovative design techniques! From basic formatting to custom graphics production, Clevid is here to design a site that is both practical and aesthetic. With years of experience building and visualizing websites, we take pride in what we create. Clevid can bring the look and feel of your business to the interactive forum of the World Wide Web. We can use any of the images, logos, or visuals you love from your existing promotional materials and integrate them into a site that reflects your current business design, or work with you to create a new concept all together. Let Clevid take your online presence to the next level!</p>
       </div><!--endCol------------------------------>
 
       <div class="col-md-4 serv">
         <h3 class="ctitle">Web Developement</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div><!--endCol------------------------------>
+          <p>Constructing and maintaining a website can often be an overwhelming task. Clevid is here to take care of all of the behind-the-scenes work that comes with developing an effective website. Our sites are designed with simplicity and stability in mind, ensuring that your website will be easy to navigate and use. We utilize state-of-the-art tools and custom coding to create sites that are unique, functional, and user-friendly. And with modern coding and design techniques, Clevid ensures that your site looks great on any device or browser. A smooth transition between interfaces is just one of the benefits of utilizing our expertise and support. Request a free consultation today to learn more about our Web Development services!</p>
+        </div><!--endCol------------------------------>
 
       <div class="col-md-4 serv">
         <h3 class="ctitle">Content Creation</h3>
@@ -158,6 +161,58 @@
 <br />
 
 <div class="row">
+  <h2 class="stitle2">Contact Us</h2>
+  <div class="col-md-6 col-md-offset-3">
+  <h3>By Phone: 832-534-3611</h3><br />
+  <h3>Or by email:</h3><br />
+
+
+      <form class="form-horizontal" role="form" method="post" action="index.php">
+        <div class="form-group">
+          <label for="name" class="col-sm-2 control-label">Name</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+            <?php echo "<p class='text-danger'>$errName</p>";?>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="email" class="col-sm-2 control-label">Email</label>
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+            <?php echo "<p class='text-danger'>$errEmail</p>";?>
+          </div>
+          <label for="number" class="col-sm-2 control-label">Number</label>
+          <div class="col-sm-10">
+            <input type="num" class="form-control" id="email" name="number" placeholder="555-555-5555" value="<?php echo htmlspecialchars($_POST['number']); ?>">
+            <!--<?php //echo "<p class='text-danger'>$errEmail</p>";?>-->
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="message" class="col-sm-2 control-label">Comment</label>
+          <div class="col-sm-10">
+            <textarea class="form-control" rows="4" name="message" placeholder="Your comments"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+            <?php echo "<p class='text-danger'>$errMessage</p>";?>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+            <?php echo "<p class='text-danger'>$errHuman</p>";?>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-10 col-sm-offset-2">
+            <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+          </div>
+        </div>
+
+      </form>
+    </div><!--endCol------------------------------>
+  </div><!--endRow-------------------------------->
+
+
+<div class="row">
   <h2 class="stitle2">Testimonials</h2>
     <div class="col-md-4">
       <h3 class="stitle">Customer 1</h3>
@@ -175,10 +230,8 @@
     </div><!--endCol------------------------------>
   </div><!--endRow-------------------------------->
 
-</div>
 
 
-</div><!--endRow---------------------------->
 
 
 
--- php/hat.php
+++ php/hat.php
@@ -65,32 +65,40 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 
 <div class="container">
 
+	<!---NavBar--->
+
+	<nav class="navbar navbar-inverse navbar-static-top">
+<div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="#">Clevid.com</a>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<ul class="nav navbar-nav">
+		<!--leftMenuHere-->
+		</ul>
+
+		<ul class="nav navbar-nav navbar-right">
+			<li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+
+		</ul>
+	</div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
 
 
-      <nav class="navbar navbar-inverse navbar-fixed-top">
-				<div class="container-fluid">
-			    <!-- Brand and toggle get grouped for better mobile display -->
-			    <div class="navbar-header">
-			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-			        <span class="sr-only">Toggle navigation</span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-			        <span class="icon-bar"></span>
-			      </button>
-
-			    </div>
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-      </nav>
-
-<div class="hat">
+	<!---endNavBar--->
 	<div class="row">
 
 	  <div class="col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-8">
@@ -102,7 +110,6 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 		    <p id="tline"><b><em>Clever Ideas</em></b> for Your Personal and Business Growth</p>
 		  </div><!--endCol---------------------------->
 		</div><!--endRow------------------------------>
-</div>
 
 
 
