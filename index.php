6445b9164363525a9c379e9c86a796a640eec8b4
Nathan <nathan@clevid.com>
2017-02-08 02:13:48 -0600

    clevid 1.1

--- index.php
+++ index.php
@@ -26,12 +26,58 @@
     </div><!--endRow-------------------------------->
 
     <div class="row">
-      <div class="col-md-5">
-        <img src="pics/responsive.png" width="100%" alt="">
-      </div>
-      <div class="col-md-7">
-        <br /><br /><br /><br /><br />
-        <h1>Responsive Design for all Device Types</h1>
+      <div class="col-md-12">
+        <!-----Carousel test--------->
+
+
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<!-- Indicators -->
+<ol class="carousel-indicators">
+  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+</ol>
+
+<!-- Wrapper for slides -->
+<div class="carousel-inner" role="listbox">
+  <div class="item active">
+    <img src="pics/responsive1.png" alt="...">
+    <div class="carousel-caption">
+      ...
+    </div>
+  </div>
+  <div class="item">
+    <img src="pics/browsers.png" alt="...">
+    <div class="carousel-caption">
+
+    </div>
+  </div>
+  <div class="item">
+    <img src="pics/seo.png" alt="...">
+    <div class="carousel-caption">
+
+    </div>
+  </div>
+
+</div>
+
+<!-- Controls -->
+<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+
+
+
+
+
+        <!--end Carousel test-------->
+
       </div>
     </div>
 
--- php/hat.php
+++ php/hat.php
@@ -51,6 +51,7 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
     <title>Clevid</title>
 
     <!-- Bootstrap -->
+		<link href="https://fonts.googleapis.com/css?family=Cabin|Nunito+Sans" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="main.css">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
